802264696c6de5ad17741af83663b14831bab04e
Mayusha04 <mayushachowdary95@gmail.com>
2022-01-20 19:29:30 +0530

    commit on master

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
 const exphbs = require("express-handlebars");
+ const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
 
 
 const employeeController = require('./controllers/employeeController');
@@ -16,10 +19,11 @@ app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine(
     "hbs",
-    exphbs.engine({
+    exphbs({
       extname: "hbs",
       defaultLayout: "mainLayout",
       layoutsDir: __dirname + "/views/layouts/",
+      handlebars: allowInsecurePrototypeAccess(Handlebars),
     })
   );
 app.set('view engine', 'hbs');
