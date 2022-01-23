69e6505dcbaa28b332a7c2130fe4ea1d8f30b15f
Mayusha04 <mayushachowdary95@gmail.com>
2022-01-23 22:47:16 +0530

    employee

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
